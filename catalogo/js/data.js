ee97f11c9cecd07aeae7f93a70ce87a15accd050
Lyroul6881 <leozinhoborges055@gmail.com>
2026-03-29 21:29:22 -0300

    Add files via upload

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Mais assistidos",
+        items: [
+            {
+                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 68,
+                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 36,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://sennashop.com/cdn/shop/articles/Senna_netflix.png?v=1727930778&width=1200", youtube: "https://www.youtube.com/watch?v=AGT5bUsOPLk", progress: 35 },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGURHZwq6vVoJJV1g7ED9rBktOOFgTWszigQ&s", youtube: "https://www.youtube.com/watch?v=lnY9FWUTY84", progress: 13 },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLqlICPfTtTLq5qCChMPTsPMmI9QSsV45Z_A&s", youtube: "https://www.youtube.com/watch?v=GroR6w3XwzY", progress: 0 },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPNUQpCFXyy3-Ja4Xynpkhjj_6GLAq6aNPUQ&s", youtube: "https://www.youtube.com/watch?v=pyi8QAlHR8k", progress: 0 },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWPlSZY03OFtRmh2QLMy1Skq6xw42CvQjYUg&s", youtube: "https://www.youtube.com/watch?v=cu2ApTImBKc", progress: 0 },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY4AF0vhg_iDXcKSo7nIxe8FU2L39MZ1-L2w&s", youtube: "https://www.youtube.com/watch?v=A7TgB4t_6Jw", progress: 10 },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGysBTa2oJ-7NvVJefbgbQ3nNEghDluUWyUQ&s", youtube: "https://www.youtube.com/watch?v=VNnb1WsNioo", progress: 0 },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz0220jls1s_N7yMqBqkGVC5wgeOkxCeHX3g&s", youtube: "https://www.youtube.com/watch?v=a6zJ9dEwNek&list=RDa6zJ9dEwNek&start_radio=1", progress: 0 },
+        ]
+    }
+];
